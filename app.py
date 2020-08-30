97e38bce278bab6768163bb74e18592e8106f96b
Stefan Jovicevic <sjovicevic@protonmail.ch>
2020-08-30 11:37:25 +0200

    Program determines itself last page, and iterates through every page to find input

--- app.py
+++ app.py
@@ -2,25 +2,41 @@
 from bs4 import BeautifulSoup
 import requests
 
-def page_num():
-    r = requests.get('https://www.kupujemprodajem.com/mobilni-telefoni/apple-iphone/grupa/23/489/1')
-    ls = []
-    soup = BeautifulSoup(r.text, 'lxml')
-    param = {'name':'data[page]'}
-    for li in soup.find_all('input', param):
-        li.get('value')
-    return ls
+# index.html is for testing purposes 
 
 
-string = 'https://www.kupujemprodajem.com/mobilni-telefoni/apple-iphone/grupa/23/489/'
-print(page_num())
-input_form = input("Search >>> ")
-input_form.lower()
-for page in range(1,3):
-    r = requests.get(string+f'{page}')
-    print(r.status_code)
+# Function that extracts contents of a page 
+def get_content(page_source, page_search, page_number):
+    page_search.lower()
+    r = requests.get(page_source)
     soup = BeautifulSoup(r.text, 'lxml')
     for match in soup.find_all('a', class_='adName', href=True):
         heading = match.text
         if input_form in heading.lower():
             print(f'{heading} - {match["href"]}')
+
+    print(f'''
+    
+    Page {page_number}
+    
+    ''')
+
+# Function for defining last page
+def page_num():
+    r = requests.get('https://www.kupujemprodajem.com/mobilni-telefoni/apple-iphone/grupa/23/489/1')
+    soup = BeautifulSoup(r.text, 'lxml')
+    return soup.find('input', {'name':'data[page]'}).get('value')
+    
+
+
+# Input for product we want to search
+input_form = input("Search >>> ")
+
+# Source page for getting number of last page
+src = 'https://www.kupujemprodajem.com/mobilni-telefoni/apple-iphone/grupa/23/489/'
+max_page = int(page_num())
+
+# Iterating through every page and extracting content that satisfies the input
+for page in range(1, max_page + 1):
+   get_content(src+f'{str(page)}', input_form, page)
+
